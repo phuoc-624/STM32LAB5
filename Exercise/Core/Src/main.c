1b3fc070c66a73d4116bfcc23754fb3a15e3c764
DESKTOP-0NSG2HJ\Admin <phuoc.nguyen622004@hcmut.edu.vn>
2025-11-14 03:40:14 +0700

    Update for Ex

--- Exercise/Core/Src/main.c
+++ Exercise/Core/Src/main.c
@@ -61,6 +61,9 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+uint8_t pData[] = "Hello\r\n";
+uint32_t testInt = 456;
+
 uint8_t temp = 0;
 uint8_t buffer[MAX_BUFFER_SIZE];
 uint8_t index_buffer = 0;
@@ -69,10 +72,10 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
 	if(huart->Instance == USART2)
 	{
-		//HAL_UART_Transmit(&huart2, &temp, 1, 50);
-		buffer[index_buffer++] = temp;
-		if(index_buffer == 30) index_buffer = 0;
-		buffer_flag = 1;
+		HAL_UART_Transmit(&huart2, &temp, 1, 50);
+		//buffer[index_buffer++] = temp;
+		//if(index_buffer == 30) index_buffer = 0;
+		//buffer_flag = 1;
 		HAL_UART_Receive_IT(&huart2, &temp, 1);
 	}
 }
@@ -109,20 +112,34 @@ int main(void)
   MX_ADC1_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  //HAL_ADC_GetValue(&hadc);
+  //char* str = itoa(HAL_ADC_GetValue(&hadc));
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  HAL_UART_Receive_IT(&huart2, &temp, 1);
+  //char str[16];
+  //uint32_t ADC_value = 0;
   while (1)
   {
+	  //HAL_UART_Transmit(&huart2, pData, sizeof(pData), 1000);
+	  //HAL_Delay(2000);
+	  //sprintf("%d", testInt);
+	  //char* str = itoa(testInt);
+	  //HAL_UART_Transmit(&huart2, str, sizeof(str), 1000);
+	  //HAL_GPIO_TogglePin(LED_YELLOW_GPIO_Port , LED_YELLOW_Pin);
+	  //ADC_value = HAL_ADC_GetValue(&hadc1);
+	  //HAL_UART_Transmit(&huart2, (void *)str, sprintf(str, "%d\n", ADC_value), 1000);
+	  HAL_Delay(500);
+	  /*
 	  if(buffer_flag == 1)
 	  {
 		  command_parser_fsm();
 		  buffer_flag = 0;
-	  }
+	  }*/
     /* USER CODE END WHILE */
-	  uart_communiation_fsm();
+	  //uart_communiation_fsm();
     /* USER CODE BEGIN 3 */
 
   }
