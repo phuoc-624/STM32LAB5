d29cb59f50e7811d8180b4e8960448dc0eae4560
DESKTOP-0NSG2HJ\Admin <phuoc.nguyen622004@hcmut.edu.vn>
2025-11-14 01:24:07 +0700

    Init STM32 Project

---
+++ Exercise/Core/Inc/fsm_automatic.h
@@ -0,0 +1,14 @@
+/*
+ * fsm_automatic.h
+ *
+ *  Created on: Nov 14, 2025
+ *      Author: Admin
+ */
+
+#ifndef INC_FSM_AUTOMATIC_H_
+#define INC_FSM_AUTOMATIC_H_
+
+
+void command_parser_fsm();
+void uart_communiation_fsm();
+#endif /* INC_FSM_AUTOMATIC_H_ */
---
+++ Exercise/Core/Src/fsm_automatic.c
@@ -0,0 +1,18 @@
+/*
+ * fsm_automatic.c
+ *
+ *  Created on: Nov 14, 2025
+ *      Author: Admin
+ */
+
+#include "fsm_automatic.h"
+
+
+void command_parser_fsm()
+{
+
+}
+void uart_communiation_fsm()
+{
+
+}
